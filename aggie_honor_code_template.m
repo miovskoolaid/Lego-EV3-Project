4fbc99f2d548403c95412b92fb284af9b9c93c35
miovskoolaid <wcmccanless@tamu.edu>
2017-02-02 09:01:01 -0600

    Add files via upload

---
+++ aggie_honor_code_template.m
@@ -0,0 +1,9 @@
+% By submitting this assignment, I agree to the following:
+% “Aggies do not lie, cheat, or steal, or tolerate those who do”
+% “I have not given or received any unauthorized aid on this assignment”
+%
+% Name: William McCanless
+% Section: 204
+% Team: 12
+% Assignment: piglatinHW5
+% Date: 11/25/2016
